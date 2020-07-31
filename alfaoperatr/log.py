7b6eaa9d2e25554f88d8340d985cc3623fe0cdbc
Andrew Cole <andrew.cole@illallangi.com>
2020-07-31 11:49:49 +1000

    E301

--- alfaoperatr/log.py
+++ alfaoperatr/log.py
@@ -44,6 +44,7 @@ class ColoredFormatter(logging.Formatter):
 class ColoredLogger(logging.Logger):
     FORMAT = "[$BOLD%(name)-50s$RESET][%(levelname)-18s]    %(message)s ($BOLD%(filename)s$RESET:%(lineno)d)"
     COLOR_FORMAT = formatter_message(FORMAT, True)
+
     def __init__(self, name):
         logging.Logger.__init__(self, name, logging.DEBUG)
 
