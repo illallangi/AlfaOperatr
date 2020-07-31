391b7130cede4c1905b51229bb2a70a38a8a0fd0
Andrew Cole <andrew.cole@illallangi.com>
2020-07-31 14:12:08 +1000

    operator.py linting

--- alfaoperatr/operator.py
+++ alfaoperatr/operator.py
@@ -1,14 +1,14 @@
 from asyncio import ensure_future, get_event_loop
 
-from .log import AlfaLog
 from .controller import AlfaController
+from .log import AlfaLog
 
 
 class AlfaOperator:
     def __init__(self, config, controller=None, logger=None):
         self.config = config
         self.controller = controller if controller else AlfaController(config)
-        self.logger = AlfaLog.get_logger(f'AlfaOperator()', self.config.log_level) if logger is None else logger
+        self.logger = AlfaLog.get_logger('AlfaOperator()', self.config.log_level) if logger is None else logger
 
     def loop(self):
         get_event_loop().run_until_complete(ensure_future(self.controller.loop()))
