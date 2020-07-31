dc9be7473f92fb90b82d813555ad12b2ab6f0306
Andrew Cole <andrew.cole@illallangi.com>
2020-07-31 10:01:58 +1000

    Added merge

--- alfaoperatr/template.py
+++ alfaoperatr/template.py
@@ -100,6 +100,7 @@ class AlfaTemplateConsumer:
     j2environment.filters["unique_dict"] = unique_dict
     j2environment.filters["cheap_hash"] = cheap_hash
     j2environment.filters["alfa_query"] = alfa_query
+    j2environment.filters["merge"] = merge
     j2environment.tests["is_subset"] = is_subset
     j2environment.tests["is_superset"] = is_superset
     return j2environment
@@ -376,6 +377,21 @@ def is_subset(v, superset):
   except (AssertionError, KeyError):
     result = False
   return result
+
+def merge(a, b, path=None):
+    "merges b into a"
+    if path is None: path = []
+    for key in b:
+        if key in a:
+            if isinstance(a[key], dict) and isinstance(b[key], dict):
+                merge(a[key], b[key], path + [str(key)])
+            elif a[key] == b[key]:
+                pass # same leaf value
+            else:
+                raise Exception('Conflict at %s' % '.'.join(path + [str(key)]))
+        else:
+            a[key] = b[key]
+    return a
 
 def ipaddr(value, action):
   if action == "revdns":
