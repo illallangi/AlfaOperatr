2feb3847f80f906b5854f2657333c534a5d6cc72
Andrew Cole <andrew.cole@illallangi.com>
2020-07-31 10:10:25 +1000

    Formatting

--- alfaoperatr/template.py
+++ alfaoperatr/template.py
@@ -383,19 +383,19 @@ def is_subset(v, superset):
   return result
 
 def merge(a, b, path=None):
-    "merges b into a"
-    if path is None: path = []
-    for key in b:
-        if key in a:
-            if isinstance(a[key], dict) and isinstance(b[key], dict):
-                merge(a[key], b[key], path + [str(key)])
-            elif a[key] == b[key]:
-                pass # same leaf value
-            else:
-                raise Exception('Conflict at %s' % '.'.join(path + [str(key)]))
-        else:
-            a[key] = b[key]
-    return a
+  "merges b into a"
+  if path is None: path = []
+  for key in b:
+    if key in a:
+      if isinstance(a[key], dict) and isinstance(b[key], dict):
+        merge(a[key], b[key], path + [str(key)])
+      elif a[key] == b[key]:
+        pass # same leaf value
+      else:
+        raise Exception('Conflict at %s' % '.'.join(path + [str(key)]))
+    else:
+      a[key] = b[key]
+  return a
 
 def ipaddr(value, action):
   if action == "revdns":
