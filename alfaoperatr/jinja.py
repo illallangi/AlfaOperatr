336cf25f5c6ff0a715e2b02fd2d9054c114d0014
Andrew Cole <andrew.cole@illallangi.com>
2020-07-31 11:50:44 +1000

    E701

--- alfaoperatr/jinja.py
+++ alfaoperatr/jinja.py
@@ -119,7 +119,8 @@ def path_join(input):
 
 def merge(a, b, path=None):
     "merges b into a"
-    if path is None: path = []
+    if path is None:
+        path = []
     for key in b:
         if key in a:
             if isinstance(a[key], dict) and isinstance(b[key], dict):
