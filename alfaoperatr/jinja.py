46ef725d5e86488485d6c1960b138381616d17fb
Andrew Cole <andrew.cole@illallangi.com>
2020-07-31 11:50:20 +1000

    E262

--- alfaoperatr/jinja.py
+++ alfaoperatr/jinja.py
@@ -125,7 +125,7 @@ def merge(a, b, path=None):
             if isinstance(a[key], dict) and isinstance(b[key], dict):
                 merge(a[key], b[key], path + [str(key)])
             elif a[key] == b[key]:
-                pass  #same leaf value
+                pass  # same leaf value
             else:
                 raise Exception('Conflict at %s' % '.'.join(path + [str(key)]))
         else:
